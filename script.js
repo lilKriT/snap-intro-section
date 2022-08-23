fbd1c01ad9d5c71f3d47b8a561cd138b1177c33a
Piotr Filipiak <kiddhkane@gmail.com>
2022-08-23 20:40:07 +0200

    Hiding mobile menu.

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 const menuButton = document.querySelector(".mobileNav .menuButton");
 menuButton.addEventListener("click", (e) => {
-    alert("clicked");
-})
+  alert("clicked");
+});
+
+const mobileMenu = document.querySelector("#mobileSidebar");
+
+const closeMobileMenuButton = document.querySelector(
+  "#close-mobile-menu-button"
+);
+closeMobileMenuButton.addEventListener("click", () => {
+  mobileMenu.classList.add("hidden");
+});
